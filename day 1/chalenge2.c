9fc1c168f97460c5e9da83983abac237b6d64858
otmane <elkorriotmane@gmail.com>
2024-09-24 17:00:24 +0200

    otmane

---
+++ day 1/chalenge2.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+
+int main () {
+
+    int C  ;
+    float K ;
+    printf("donner la tempirature en celesius : ");
+    scanf("%d",&C);
+    K= C + 273.15 ;
+    printf("la tempirature en K est : %.2f ",K);
+
+        
+        
+   
+    
+    
+    
+    
+    
+
+    return 0;
+}
---
+++ day 1/chalenge3.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+
+int main() {
+    float Km  ;
+    float yards ;
+    printf("donner la distance en kilomitrage  : ");
+    scanf("%f",&Km);
+    yards= Km * 1093.61 ;
+    printf("la distance en yards  est : %.2f yards ",yards);
+
+    return 0;
+
+    
+    
+    
+    
+    
+
+    return 0;
+}
